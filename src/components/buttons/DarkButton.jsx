8333db41e5221bc55721334db3aad26fe1ff2ece
Java-rice <johnmark.p.peroche@gmail.com>
2024-06-06 09:18:20 +0800

    hero and navbar

---
+++ src/components/buttons/DarkButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DarkButton = () => {
+  return (
+    <div>
+        <button type="button" className="bg-indigo-600 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-300">
+            Search
+        </button>
+    </div>
+  )
+}
+
+export default DarkButton
