920eda16bc78efc845b1176fac47410b7db59549
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2024-06-07 19:52:28 +0800

    dad

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import './App.css';
