4827c08f323d609b6c3649a164f0ea99e1faa928
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2024-06-14 23:10:14 +0800

    register part, but without validation plus without the shadow part at the top

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import "./Register.css";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import Shadow from "../../assets/shadow-above.png";
 
 const Register = () => {
   return (
     <div className="register-container">
         <div className="register-content">
+            <div className="shadow-above"></div>
+            
             <div className="header-container">
                 <h1>Input your information</h1>
                 <p className="header-p">We need you to help us with some basic information for your account creation. Here are our <span style={{color: "#4743E0"}}>terms and conditions</span>. Please read them carefully. We are GDPR compliant</p>
@@ -68,8 +71,10 @@ const Register = () => {
                     <input type="password" className="password-input" required/>
                 </div>
             </div>
-
-            <button className="register-button">Register</button>
+            
+            <div className="button-container">
+                <button className="register-button">Register</button>
+            </div>
         </div>
 
         <div className="register-bg"></div>
