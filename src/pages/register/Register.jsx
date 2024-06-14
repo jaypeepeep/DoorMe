2c3ebb654915adff446d766aace65ab52837cf9b
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2024-06-14 18:41:37 +0800

    cdadad

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Register.css";
+
+function Register() {
+    return(
+        <div className="register-container">
+            <div className="register-content">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Register;
