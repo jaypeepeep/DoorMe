e133f1579b3c943bc5682e725e95f8114ac7992a
John Patrick Lagatuz <patrick.zutagal@gmail.com>
2024-06-15 09:51:52 +0800

    added about sections

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about-container">
+      <div className="about-section1-container">
+        <div className="about-section3-content"></div>
+      </div>
+      <div className="about-section2-container">
+        <div className="about-section3-content"></div>
+      </div>
+      <div className="about-section3-container">
+        <div className="about-section3-content"></div>
+      </div>
+      <div className="about-section4-container">
+        <div className="about-section4-content"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
