1523bec5cdf3d8940e0f21291da6b52eba412120
John Patrick Lagatuz <patrick.zutagal@gmail.com>
2024-06-15 12:38:03 +0800

    about progress - faq last one

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./About.css";
 import aboutsection2img from "../../assets/about-section2.jpg";
 import aboutsection2icon from "../../assets/about-section2-icon.png";
+import aboutsection3icon from "../../assets/about-section3-icon.png";
+import aboutsection3column2 from "../../assets/about-section3-column2.png";
+import aboutsection3column3 from "../../assets/about-section3-column3.png";
+import aboutsection3column4 from "../../assets/about-section3-column4.png";
 
 function About() {
   return (
@@ -30,7 +34,10 @@ function About() {
             />
             <div className="about-section2-text">
               <div className="about-section2-title">
-                <img src={aboutsection2icon} alt="Three young men in a dorm." />
+                <img
+                  src={aboutsection2icon}
+                  alt="Purple square icon with rounded corners."
+                />
                 <h2>About Us</h2>
               </div>
               <h1>Our Mission</h1>
@@ -53,7 +60,28 @@ function About() {
         </div>
       </div>
       <div className="about-section3-container">
-        <div className="about-section3-content"></div>
+        <div className="about-section3-content">
+          <div className="about-section3-first-column">
+            <div className="about-section3-title">
+              <img src={aboutsection3icon} alt="Purple cube icon." />
+              <h2>DoorMe Features</h2>
+            </div>
+            <h1>Your Ultimate Dorm Hunting Companion</h1>
+            <button className="hunt-now-button">Hunt Now</button>
+          </div>
+          <div className="about-section3-other-column">
+            <img src={aboutsection3column2} alt="A star shape icon." />
+            <h1>Comprehensive Listings</h1>
+          </div>
+          <div className="about-section3-other-column">
+            <img src={aboutsection3column3} alt="A personalized search icon." />
+            <h1>Personalized Search</h1>
+          </div>
+          <div className="about-section3-other-column">
+            <img src={aboutsection3column4} alt="A shield or badge icon." />
+            <h1>Secure and Easy </h1>
+          </div>
+        </div>
       </div>
       <div className="about-section4-container">
         <div className="about-section4-content"></div>
